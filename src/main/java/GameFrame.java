0c43c6e5920d65a1b53b151575689f3dab36d47b
Yaroslav <yaroslav.ilin@icloud.com>
2024-05-17 01:03:42 +0300

    first commit

---
+++ src/main/java/GameFrame.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+    GamePanel panel;
+    GameFrame() {
+        panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Pong");
+        this.setResizable(false);
+        this.setBackground(Color.black);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
